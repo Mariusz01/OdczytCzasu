18307ca0a8e9ecc90262fa8e8d9663a61cdba728
Mariusz01 <45500049+Mariusz01@users.noreply.github.com>
2020-05-09 01:07:45 +0200

    Dodaj pliki projektów.

---
+++ Pages/Czas2.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdczytCzasu.Pages
+{
+    public class Czas2Model : PageModel
+    {
+        public void OnGet()
+        {
+            var dateData = DateTime.UtcNow.ToString();
+            ViewData["dateData"] = dateData;
+        }
+    }
+}
